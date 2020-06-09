03cd3fd9d4e5f1aee086009a2ce962bb939bf9a5
Ryan Street <ryan@coolryan.com>
2020-06-09 13:37:16 -0400

    added Object Scaffolding

---
+++ objects/User.js
@@ -0,0 +1,30 @@
+/**
+ * User Object
+ *
+ * All user based functionality.
+ */
+
+module.exports = {
+
+    /**
+     * Get user league profile
+     */
+    profile() {
+
+    },
+
+    /**
+     * Shows recent matches
+     */
+    recent() {
+
+    },
+
+    /**
+     * Shows currently registered Deck and Alias
+     */
+    currentDeck() {
+
+    }
+
+}
