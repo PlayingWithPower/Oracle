46d204dfffedb98e5d8c13a9a983ef7e4c8edff5
saldanan1 <31412910+saldanan1@users.noreply.github.com>
2020-06-09 19:44:30 -0400

    Small fix

--- objects/User.js
+++ objects/User.js
@@ -10,7 +10,7 @@ module.exports = {
      * Get user league profile
      */
     profile(receivedMessage, args) {
-    const user = require('../Schema/Users')
+        const user = require('../Schema/Users')
         let query = {_name: receivedMessage.author.username}
         user.findOne(query, function(err, res){
             console.log(res)
